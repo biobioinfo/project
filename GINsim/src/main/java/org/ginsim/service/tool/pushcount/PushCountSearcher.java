d5503963e2628eea2d0966594f74bf0e607d2ad9
mheinric <marc.heinrich@free.fr>
2015-01-28 17:55:49 +0100

    Works on simple tests, interface remains basic
    Pushcount seems to give the appropriate results, some work remain on
    the interface to use it more easily (and see the results)
    For now prints on stdout.

--- GINsim/src/main/java/org/ginsim/service/tool/pushcount/PushCountSearcher.java
+++ GINsim/src/main/java/org/ginsim/service/tool/pushcount/PushCountSearcher.java
@@ -2,226 +2,141 @@ package org.ginsim.service.tool.pushcount;
 
 
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 import org.colomoto.common.task.AbstractTask;
 import org.colomoto.logicalmodel.LogicalModel;
 import org.colomoto.mddlib.MDDManager;
-import org.colomoto.mddlib.MDDManagerFactory;
-import org.colomoto.mddlib.MDDVariable;
-import org.colomoto.mddlib.MDDVariableFactory;
-import org.colomoto.mddlib.operators.MDDBaseOperators;
 
 public class PushCountSearcher extends AbstractTask<Object> {
 	
 	private LogicalModel model ;
 	
-	//Temporary manager with more variables
-	private MDDManager tempManager ;
-	//Number of variables in the initial manager
-	private int varNum ;
-	
-	//List of the new variables
-	private List<MDDVariable> varList = new ArrayList<MDDVariable>() ;
-	
-	//Copy of the functions into the tempManager
-	int[] tempFunc ;
+	private final StateTransitionGraph trGraph ;
 	
 	//BDD representing the source states
-	int tempSource ;
+	private final int source ;
 	
 	//BDD representing the target states
-	int tempTarget ;
+	private int target ;
 
 	public PushCountSearcher(LogicalModel model, int source, int target) throws ParseException {
 		this.model = model ;
 		MDDManager manager = model.getMDDManager() ;
-
-		MDDVariable[] vars = manager.getAllVariables() ;
-		varNum = vars.length ;
-		String[] tempVars = new String[vars.length] ;
-		for(int i = 0 ; i < tempVars.length ; i++)
-			tempVars[i] = "temp " + i ;
 		
-		MDDVariableFactory allVars = new MDDVariableFactory() ;
+		trGraph = new StateTransitionGraph(model) ;
 		
-		for(int i = 0 ; i < vars.length ; i++)
-			allVars.add(vars[i], vars[i].nbval) ;
-		for(int i = 0 ; i < vars.length ; i++)
-			allVars.add(tempVars[i], vars[i].nbval) ;
-		
-		tempManager = MDDManagerFactory.getManager(allVars, manager.getLeafCount()) ;
-		
-		for(int i = 0 ; i < 2* varNum ; i ++)
-			varList.add(tempManager.getVariableForKey(allVars.get(i))) ;
-		
-		int[] func = model.getLogicalFunctions() ;
-		tempFunc = new int[func.length] ;
-		for(int i = 0 ; i<func.length ; i++)
-			tempFunc[i] = tempManager.parseDump(manager.dumpMDD(func[i])) ;
-		
-		tempSource = tempManager.parseDump(manager.dumpMDD(source)) ;
-		tempTarget = tempManager.parseDump(manager.dumpMDD(target)) ;
+		this.source = source ;  
+		this.target = target ;
 		
 	}
 
 	@Override
 	public Object doGetResult() throws Exception {
 		System.out.println("Running") ;
-		int async = getAsynchronousTransitionFunction() ;
-		System.out.println("Computed f_async") ;
-		int bdd = getPredecessors(async, tempTarget) ;
-		System.out.println("Computed predecessors") ;
-		bdd = model.getMDDManager().parseDump(tempManager.dumpMDD(bdd)) ;
+		Iterator<byte[]> iter0 = AssignmentsEnum.getAssignments(model.getMDDManager(), target) ;
+		int f = trGraph.getAsynchronousTransitionFunction() ;
+		System.out.println("From :=") ;
+		Iterator<byte[]> iter2 = AssignmentsEnum.getAssignments(model.getMDDManager(), source) ;
+		while(iter2.hasNext())
+			printState(iter2.next()) ;
+		System.out.println();
 		
-		System.out.println(model.getMDDManager().dumpMDD(bdd)) ;
-		for(int i = 0 ; i < varNum ; i++)
-			System.out.println(i + " : " + varList.get(i) ) ;
-		
-		int iter = 0 ;
-		Iterator<byte[]> it = AssignmentsEnum.getAssignments(model.getMDDManager(), bdd) ;
-		while(it.hasNext())
+		while(iter0.hasNext())
 		{
-			byte[] r = it.next() ;
-			System.out.print("res: ") ;
-			for(int i = 0 ; i < r.length ; i++)
-				System.out.print(r[i]) ;
-			System.out.println() ;
-			iter ++ ;
-			if(iter > 100)
-				return null; 
+			byte[] target = iter0.next();
+			System.out.println("To go to ") ;
+			printState(target) ;
+			int statemdd = model.getMDDManager().nodeFromState(target, 1) ;
+			statemdd = trGraph.getManager().parseDump(model.getMDDManager().dumpMDD(statemdd)) ;
+			
+			int br = trGraph.getBR(f, statemdd) ;
+			br = model.getMDDManager().parseDump(trGraph.getManager().dumpMDD(br)) ;
+			
+			byte[] closestPred = getClosest(f, model.getMDDManager(), source, 
+					br, target) ;
+			System.out.println("go up to the state") ;
+			printState(closestPred) ;
+			System.out.println("with a push of " + dist(model.getMDDManager(), source, closestPred)) ;
+			System.out.println("-------------") ;
 		}
 		return null ;
 	}
 	
 	/**
-	 * An MDD representing the transition function. The resulting function takes two 
-	 * bit-string arguments x and y, and returns 1 if there is a transition from x to y, 
-	 * and 0 otherwise
-	 * @return
+	 * Utility function to print a state on the standard output
+	 * @param state
 	 */
-	int getSynchronousTransitionFunction(){
-		int[] fi = new int[tempFunc.length] ;
-		MDDVariable[] xVars = varList.subList(0, varNum).toArray(new MDDVariable[0]) ;
-		MDDVariable[] yVars = varList.subList(varNum, 2*varNum).toArray(new MDDVariable[0]) ;
-		
-		
-		for(int i = 0 ; i < fi.length ; i ++)
-		{
-			int xi = getVariableValue(xVars[i]) ;
-			int yi = getVariableValue(yVars[i]) ;
-			int i1 = MDDBaseOperators.AND.combine(tempManager, 
-					SimpleOperator.EQUAL.combine(tempManager, tempFunc[i], xi),
-					SimpleOperator.EQUAL.combine(tempManager, xi, yi));
-			
-			int i2 = MDDBaseOperators.AND.combine(tempManager, 
-					SimpleOperator.GREATER_THAN.combine(tempManager, tempFunc[i], xi),
-					SimpleOperator.EQUAL.combine(tempManager, 
-							SimpleOperator.ADD.combine(tempManager, xi, 1), yi)) ;
-			int i3 = MDDBaseOperators.AND.combine(tempManager, 
-					SimpleOperator.LESS_THAN.combine(tempManager, tempFunc[i], xi),
-					SimpleOperator.EQUAL.combine(tempManager, 
-							SimpleOperator.SUB.combine(tempManager, xi, 1), yi)) ;
-			fi[i] = MDDBaseOperators.OR.combine(tempManager, new int[]{i1 , i2, i3}) ;
-			System.out.println(tempManager.dumpMDD(fi[i])) ;
-		}
-		//Function whose value is true on x,y iff (x,y) is a transition
-		int f = MDDBaseOperators.AND.combine(tempManager, fi) ;
-		
-		return f ;
+	private void printState(byte[] state) {
+		for(int i = 0 ; i < state.length ; i++)
+			System.out.print(state[i]) ;
+		System.out.println() ;
 	}
 	
 	/**
-	 * An MDD representing the transition function. The resulting function takes two 
-	 * bit-string arguments x and y, and returns 1 if there is a transition from x to y, 
-	 * and 0 otherwise
+	 * Returns the minimum distance from one state of set to state.
+	 * The set must not be empty
+	 * @param m
+	 * @param set
+	 * @param state
 	 * @return
 	 */
-	int getAsynchronousTransitionFunction(){
-		//We can write f = OR(fi)
-		//Where the fi are defined by :
-		//fi = AND_{j \neq i} (x_j = y_j) 
-		//	AND ((y_i = x_i AND x_i = f_i(x)) 
-		//			OR (x_i < f_i(x) AND y_i = x_i +1) 
-		//			OR x_i > f_i(x) AND y_i = x_i -1)
-		
-		int[] fi = new int[tempFunc.length] ;
-		for(int i = 0 ; i < fi.length ; i ++)
+	private int dist(MDDManager m, int set, byte[] state){
+		Iterator<byte[]> it = AssignmentsEnum.getAssignments(m, set) ;
+		int minDist = Integer.MAX_VALUE ;
+		while(it.hasNext())
+			minDist = Math.min(minDist, dist(state, it.next())) ;
+		return minDist ;
+	}
+	
+	/**
+	 * The distance between the two states
+	 * @param m
+	 * @param state1
+	 * @param state2
+	 * @return
+	 */
+	private int dist(byte[] state1, byte[] state2){
+		assert(state1.length == state2.length) ;
+		int d = 0 ;
+		for(int i = 0 ; i < state1.length ; i ++)
+			d += Math.abs(state1[i] - state2[i]) ;
+		return d ;
+	}
+	
+	/**
+	 * Returns the state in targetSet that is the closest (in term of push) 
+	 * from a state in source.
+	 * If there are several at the same distance, then the one that corresponds to 
+	 * the shortest path to target is the kept.
+	 * The MDD manager corresponds to the manager used to represent the source and 
+	 * target sets. 
+	 * The transition function is on the other hand connected to the Manager of the trGraph
+	 * @return
+	 */
+	private byte[] getClosest(int f, MDDManager m, int source, int targetSet, byte[] target){
+		byte[] res = null ;
+		int minDist = m.getLeafCount() * trGraph.varNum ;
+		int minPathDist = Integer.MAX_VALUE ;
+		Iterator<byte[]> it = AssignmentsEnum.getAssignments(m, targetSet) ;
+		while(it.hasNext())
 		{
-			int[] fij = new int[varNum] ;
-			for(int j = 0 ; j < varNum ; j ++)
+			byte[] next = it.next() ;
+			int d = dist(m, source, next) ;
+			
+			if(minDist >= d)
 			{
-				if(j != i)
-					fij[j] = SimpleOperator.EQUAL.combine(tempManager, 
-							getVariableValue(varList.get(j)), 
-							getVariableValue(varList.get(varNum + j))) ;
-				else
+				int newPathDist = trGraph.pathDist(f, next, target) ; 
+				if(minDist > d || newPathDist < minPathDist)
 				{
-					int xi = getVariableValue(varList.get(i)) ;
-					int yi = getVariableValue(varList.get(i + varNum)) ;
-					
-					int i1 = MDDBaseOperators.AND.combine(tempManager,
-							SimpleOperator.EQUAL.combine(tempManager, xi, yi),
-							SimpleOperator.EQUAL.combine(tempManager, xi, tempFunc[i])) ;
-					int i2 = MDDBaseOperators.AND.combine(tempManager,
-							SimpleOperator.GREATER_THAN.combine(tempManager, tempFunc[i], xi),
-							SimpleOperator.EQUAL.combine(tempManager, 
-									SimpleOperator.ADD.combine(tempManager, xi, 1),
-									yi)) ;
-					int i3 = MDDBaseOperators.AND.combine(tempManager,
-							SimpleOperator.LESS_THAN.combine(tempManager, tempFunc[i], xi),
-							SimpleOperator.EQUAL.combine(tempManager, 
-									SimpleOperator.SUB.combine(tempManager, xi, 1),
-									yi)) ;
-					
-					fij[j] = MDDBaseOperators.OR.combine(tempManager, new int[]{i1,i2,i3}) ;
+					minDist = d ;
+					res = next ;
+					minPathDist = newPathDist ;
 				}
 			}
-			fi[i] = MDDBaseOperators.AND.combine(tempManager, fij) ;
 		}
-		
-		return MDDBaseOperators.OR.combine(tempManager, fi) ;
+		return res;
 	}
 	
-	/**
-	 * Produces a MDD that returns the value of the variable var
-	 * (does not depend on other variables).
-	 * @param var
-	 * @return
-	 */
-	int getVariableValue(MDDVariable var){
-		int[] values = new int[var.nbval] ;
-		for(int i = 0 ; i < var.nbval ; i++)
-		{
-			values[i] = i ;
-		}
-		return var.getNode(values) ;
-	}
-	
-	/**
-	 * Returns an MDD that represents the set of states that can reach one of 
-	 * the states described by startStates in 0, 1, or more transitions
-	 * @param f : the transition function
-	 * @param startStates : the set of initial states
-	 * @return
-	 */
-	int getPredecessors(int f, int startStates){
-		int oldSet = 0 ;
-		int newSet = startStates ;
-		MDDVariable[] xVars = varList.subList(0, varNum).toArray(new MDDVariable[0]) ;
-		MDDVariable[] yVars = varList.subList(varNum, 2*varNum).toArray(new MDDVariable[0]) ;
-		while(oldSet != newSet)
-		{
-			oldSet = newSet ;
-			newSet = SimpleOperator.substitute(tempManager, xVars, yVars, newSet) ;
-			newSet = SimpleOperator.MIN.combine(tempManager, f, newSet) ;
-			newSet = MDDQuantifier.QUANTIFY_MAX.combine(tempManager, yVars, newSet) ;
-			newSet = MDDBaseOperators.OR.combine(tempManager, newSet, oldSet) ;
-		}
-			
-		return newSet ;
-	}
 
 }
