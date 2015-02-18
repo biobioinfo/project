7424079c2ea5e49b6079ab96f8899ea64e9e9f70
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2015-02-18 13:43:03 +0100

    Adding missing part of code which find a maximal seed

--- GINsim/src/main/java/org/ginsim/service/tool/maximalsymbolicsteadystates/MaximalSymbolicSteadyStatesService.java
+++ GINsim/src/main/java/org/ginsim/service/tool/maximalsymbolicsteadystates/MaximalSymbolicSteadyStatesService.java
@@ -42,7 +42,7 @@ public class MaximalSymbolicSteadyStatesService implements Service {
 	return;
     }
     
-    public void run() {	
+    public void run() {
 	// Enumerate and print MDD implicants
 	enumerateMDDImplicants();
 	System.out.println("");
@@ -57,7 +57,11 @@ public class MaximalSymbolicSteadyStatesService implements Service {
 	
 	// Solve the Integer Linear Programming
 	System.out.println("");
-	solveILP();
+	try {
+	    solveILP();
+	} catch (Exception exception) {
+	    // Don't care
+	}
 	
 	return;
     }
@@ -264,12 +268,20 @@ public class MaximalSymbolicSteadyStatesService implements Service {
     
     
     // Solve the Integer Linear Programming
-    private void solveILP() {
+    private void solveILP() throws Exception {
+	
+	Implicant implicant;
+	ListIterator<Implicant> implicantIterator;
 	
 	// Create variables x_a in {0, 1} for every arc a = (p, c, v)
 	List<Variable> x = new ArrayList<Variable>();
-	for (int a = 0; a < this.primeImplicants.size(); a++) {
-	    // Create variable x
+	implicantIterator = this.primeImplicants.listIterator();
+	while (implicantIterator.hasNext()) {
+	    // Tattoo prime implicant with the correct index
+	    int a = implicantIterator.nextIndex();
+	    implicant = implicantIterator.next();
+	    implicant.index = a;
+	    // Create variable x_a
 	    Variable x_a = Variable.make("x_" + a);
 	    x_a.lower(0);
 	    x_a.upper(1);
@@ -308,41 +320,61 @@ public class MaximalSymbolicSteadyStatesService implements Service {
 	
 	// Create a model and add variables to it
 	ExpressionsBasedModel model = new ExpressionsBasedModel();
-	ListIterator<Variable> iterator = x.listIterator();
-	while (iterator.hasNext()) {
-	    Variable variable = iterator.next();
+	ListIterator<Variable> variableIterator = x.listIterator();
+	while (variableIterator.hasNext()) {
+	    Variable variable = variableIterator.next();
 	    model.addVariable(variable);
 	}
-	iterator = y0.listIterator();
-	while (iterator.hasNext()) {
-	    Variable variable = iterator.next();
+	variableIterator = y0.listIterator();
+	while (variableIterator.hasNext()) {
+	    Variable variable = variableIterator.next();
 	    model.addVariable(variable);
 	}
-	iterator = y1.listIterator();
-	while (iterator.hasNext()) {
-	    Variable variable = iterator.next();
+	variableIterator = y1.listIterator();
+	while (variableIterator.hasNext()) {
+	    Variable variable = variableIterator.next();
 	    model.addVariable(variable);
 	}
 	
-	/**
 	// Create constraints  C1: for all c and v_i, yc_i = (Or_{a in Bc_i} x_a)
-	// (i.e. | for all a in Bc_i, x_a <= yc_i   )
-	// (     | 0 <= (Sum_{a in Bc_i} x_a) - yc_i)
+	// (i.e. | for all a in Bc_i, x_a <= yc_i      (I))
+	// (     | 0 <= (Sum_{a in Bc_i} x_a) - yc_i  (II))
 	Collection<List> partition = partitionImplicants(this.primeImplicants);
 	Iterator<List> partIterator = partition.iterator();
 	while (partIterator.hasNext()) {
-	    List part = partIterator.next();
-	    ListIterator<Implicant> iterator = part.listIterator();
-	    while (iterator.hasNext()) {
-		Implicant implicant = iterator.next();
-		Expression expression = model.addExpression("C1_" + implicant.c + "," + implicant.v);
+	    List<Implicant> part = partIterator.next();
+	    // Create constraint (II)
+	    implicant = part.get(0);
+	    Variable  yc_i;
+	    switch (implicant.c) {
+	    case 0:
+		yc_i = y0.get(implicant.v);
+		break;
+	    case 1:
+		yc_i = y1.get(implicant.v);
+		break;
+	    default:
+		throw new Exception("Illegal implicant (illegal third component)");
+	    }
+	    Expression partExpression = model.addExpression("C1_" + implicant.c + "," + implicant.v);
+	    partExpression.lower(0);
+	    partExpression.setLinearFactor(yc_i, -1);
+	    // Create constraint (I)
+	    implicantIterator = part.listIterator();
+	    while (implicantIterator.hasNext()) {
+		// Create subconstraint for constraint (I)
+		implicant = implicantIterator.next();
+		Expression expression = model.addExpression("C1_" + implicant.c + "," + implicant.v + "," + implicant.index);
 		expression.upper(0);
-		
+		Variable x_a = x.get(implicant.index);
+		expression.setLinearFactor(x_a, 1);
+		expression.setLinearFactor(yc_i, -1);
+		// Complete constraint (II)
+		partExpression.setLinearFactor(x_a, 1);
 	    }
 	}
-	*/
 	
-	// Create constraints  C2: for all i, not(y0_i) or not(y1_i)
+	// Create constraints  C2: for all v_i, not(y0_i) or not(y1_i)
 	// (i.e. y0_i + y1_i <= 1)
 	for (int i = 0; i < numberOfGenes(); i++) {
 	    Expression expression = model.addExpression("C2_" + i);
@@ -353,14 +385,41 @@ public class MaximalSymbolicSteadyStatesService implements Service {
 	    expression.setLinearFactor(y1_i, 1);
 	}
 	
-	// Create constraints C3
-	// TODO: paste corresponding code
+	// Create constraints C3: for all a, for all v_i in U_{T(a)}, not(x_a) or y(T(a)(v_i))_i
+	// (i.e. y(T(a)(v_i))_i - x_a >= 0)
+	implicantIterator = this.primeImplicants.listIterator();
+	while (implicantIterator.hasNext()) {
+	    implicant = implicantIterator.next();
+	    Variable x_a = x.get(implicant.index);
+	    for (int v = 0; v < numberOfGenes(); v++) {
+		int c = implicant.p[v];
+		if (-1 != c) {
+		    Variable  yc_i;
+		    switch (c) {
+		    case 0:
+			yc_i = y0.get(v);
+			break;
+		    case 1:
+			yc_i = y1.get(v);
+			break;
+		    default:
+			throw new Exception("Illegal partial state (illegal value)");
+		    }
+		    Expression expression = model.addExpression("C3_" + implicant.index + "," + v);
+		    expression.lower(0);
+		    expression.setLinearFactor(x_a, -1);
+		    expression.setLinearFactor(yc_i, 1);
+		} else {
+		    // Don't care
+		}
+	    }
+	}
 	
 	// Solve the problem (i.e. maximise the cost)
 	Optimisation.Result result = model.maximise();
 	
-	// Create constraints C4
-	// TODO: paste corresponding code
+	// Create constraints C4: ...
+	// TODO: complete
 	
 	// Print the result and the model
 	BasicLogger.debug();
